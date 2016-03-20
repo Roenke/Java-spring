af015b7aba56226573f81528809e891fd7e92472
Roenke <roenke54@gmail.com>
2016-03-20 11:32:57 +0300

    Add tests stub for Collections. Implement one test for map Collections.map function.

---
+++ ha/functional-java/src/test/java/CollectionsTest.java
@@ -0,0 +1,60 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+public class CollectionsTest {
+
+    @Test
+    public void map() throws Exception {
+        ArrayList<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+
+        Function1<Integer, Integer> square = value -> value * value;
+
+        Collection<Integer> result = iterableToCollection(Collections.map(square, intList));
+        Integer[] expected = new Integer[]{1, 4, 9};
+
+        assertEquals(3, result.size());
+        assertTrue(result.containsAll(Arrays.asList(expected)));
+    }
+
+    @Test
+    public void filter() throws Exception {
+
+    }
+
+    @Test
+    public void takeWhile() throws Exception {
+
+    }
+
+    @Test
+    public void takeUnless() throws Exception {
+
+    }
+
+    @Test
+    public void foldl() throws Exception {
+
+    }
+
+    @Test
+    public void foldr() throws Exception {
+
+    }
+
+    private static <T> Collection<T> iterableToCollection(Iterable<T> iterable) {
+        Collection<T> collection = new ArrayList<>();
+        for (T elem : iterable) {
+            collection.add(elem);
+        }
+
+        return collection;
+    }
+}
